6ddcb88b55b497033c21f2e65ead7cf5edd48497
Abrar Akhand <Jahin345@gmail.com>
2021-05-03 21:17:51 -0700

    testing react stuff

---
+++ audio-visualizer-app/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const api = {
+  key: '267d01ce1338a25fbe4081069b694cd7',
+  base: 'https://api.openweathermap.org/data/2.5/'
+}
+
+
+
+
+function App() {
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+          />
+        </div>
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
