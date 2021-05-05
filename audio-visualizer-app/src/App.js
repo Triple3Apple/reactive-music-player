d3f17fe8dcf11e9b853278dbab73034c557757f9
Abrar Akhand <Jahin345@gmail.com>
2021-05-04 21:38:01 -0700

    Basic react tut page set up

--- audio-visualizer-app/src/App.js
+++ audio-visualizer-app/src/App.js
@@ -11,6 +11,19 @@ const api = {
 
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  }
+
   return (
     <div className="app">
       <main>
@@ -20,6 +33,20 @@ function App() {
             className="search-bar"
             placeholder="Search..."
           />
+        </div>
+        <div className="location-box">
+          <div className="location">
+            Arizona, US
+          </div>
+          <div className="date">{dateBuilder(new Date())}</div>
+          <div className="weather-box">
+            <div className="temp">
+              15°C
+            </div>
+            <div className="weather">
+              Sunny
+            </div>
+          </div>
         </div>
       </main>
 
